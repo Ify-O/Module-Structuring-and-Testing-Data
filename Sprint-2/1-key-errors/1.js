7e81f17e8b3ab059dfaa7a4f4fe178e0e9f16532
Ifeoma <osegboifeoma3@gmail.com>
2025-12-17 15:00:07 +0100

    reviewed, explained and updated the codes.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,11 +2,11 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+    //the error will happen because there is a second assignation of the same variable "decimalNumber" using const after it has been declared in the function.
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
@@ -15,6 +15,16 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+    // as predicted, it says on line 10 there is an error. it explains it's a syntax error because the identifier 'decimalNumber' has been declared. Also just removing the keyword 'const' doesn't solve the problem. The declaration of the variable on line 10 doesn't mean its declared for the the console.log to log it in the terminal. itt needs to be declared in the console, so we have tto remove the repetition.
+    // Also on line 15, the console.log needs to call the input of the function and not the variable.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
+
