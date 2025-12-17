fb30eda00293cf343b4b0a5c7f834b7a757db509
Ifeoma <osegboifeoma3@gmail.com>
2025-12-17 20:50:13 +0100

    created a function to add SnakeCase and UpperCase

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,13 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+    //My code
+
+function changeCase(str) {
+  let addSnakeCase = str.replace(/\s+/g, "_");
+  let capitalize = addSnakeCase.toUpperCase();
+  return capitalize;
+}
+
+console.log(changeCase('i am beautiful'));
