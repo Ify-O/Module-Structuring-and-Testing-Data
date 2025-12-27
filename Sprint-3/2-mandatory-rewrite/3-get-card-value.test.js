0aac1eba97e8bb788c99543c3c6fc3f3ca2961ca
Ifeoma <osegboifeoma3@gmail.com>
2025-12-27 19:27:25 +0100

    Add tests for card values and error handling in getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,31 @@
 const getCardValue = require("./3-get-card-value");
 
+//Case 1: Handle Ace (A):
 test("should return 11 for Ace of Spades", () => {
     const aceofSpades = getCardValue("A♠");
     expect(aceofSpades).toEqual(11);
     });
-
 // Case 2: Handle Number Cards (2-10):
+test("should return 8 for 8 of Hearts", () => {
+  const eightOfHearts = getCardValue("8♥");
+  expect(eightOfHearts).toEqual(8);
+});
 // Case 3: Handle Face Cards (J, Q, K):
-// Case 4: Handle Ace (A):
+test("should return J for jack of Spades",() => {
+    const JackOfSpades = getCardValue("J♠");
+    expect(JackOfSpades).toEqual(10);
+});
+test("should return Q for Queen of Diamonds", () => {
+  const queenofDiamonds = getCardValue("Q♦");
+  expect(queenofDiamonds).toEqual(10);
+});
+test("should return K for King of Spades", () => {
+  const kingOfSpades = getCardValue("K♠");
+  expect(kingOfSpades).toEqual(10);
+});
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+  expect(() => {
+    getCardValue("F♠");
+  }).toThrow("Invalid card rank");
+});
